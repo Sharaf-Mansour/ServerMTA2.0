b3981ad7152f14725a2238d7aecb3a5d05a0fd69
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2020-10-25 15:20:06 +0200

    Test auto update

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -7,7 +7,7 @@ self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
-const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const cacheName = `${cacheNamePrefix}${self.assetsManifest.version++}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/ ];
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
